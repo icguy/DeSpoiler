63fd6c4153bc46d2fdc57015b33eff29718da491
icguy <ayhandani@hotmail.com>
2018-05-01 22:01:51 +0200

    initalized project, set up firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+	constructor(
+		private readonly db: AngularFireDatabase
+	) {
+
+	}
+
+	ngOnInit(): void {
+		let items = this.db.list("/items")
+			.valueChanges()
+			.subscribe(val =>
+				console.log(val)
+			);
+	}
 }
