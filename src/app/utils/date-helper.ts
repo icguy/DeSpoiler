6a136e32d0d9b41fc52c6085b602080be1014977
icguy <ayhandani@hotmail.com>
2018-05-25 23:53:22 +0200

    fixed bug in date-helper.ts

--- src/app/utils/date-helper.ts
+++ src/app/utils/date-helper.ts
@@ -1,7 +1,7 @@
 import * as moment from "moment";
 
 export function getDaysUntil(date: moment.Moment): number {
-	let originalDate = date.startOf("day");
+	let originalDate = moment(date).startOf("day");
 	let now = moment().startOf("day");
 	return originalDate.diff(now, "day");
 }
