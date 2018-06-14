a3506f29f5b379dd2640409a423fdfbfae296bdb
icguy <ayhandani@hotmail.com>
2018-06-15 00:36:27 +0200

    fixed prod build and removed debug buttons

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  		apiKey: "AIzaSyAFAUBCnrfJ5q2gG3YQuNvYPlrppW9Egeg",
+  		authDomain: "despoiler-583f0.firebaseapp.com",
+  		databaseURL: "https://despoiler-583f0.firebaseio.com",
+  		projectId: "despoiler-583f0",
+  		storageBucket: "despoiler-583f0.appspot.com",
+  		messagingSenderId: "48167174818"
+  	} 
 };
