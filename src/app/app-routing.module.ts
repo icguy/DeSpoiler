5a15f1bc13e411c3a321d221343389836aa54fe6
ayhand <ayhandani@hotmail.com>
2021-04-21 00:34:40 +0200

    new angular infrastructure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { ItemDetailComponent } from "./pages/item-detail/item-detail.component";
-import { ItemListComponent } from "./pages/item-list/item-list.component";
-import { AuthGuard } from "./shared/auth.guard";
-import { LoginComponent } from "./pages/login/login.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-	{ path: "", redirectTo: "/list", pathMatch: "full", canActivate: [AuthGuard] },
-	{ path: "list", component: ItemListComponent, canActivate: [AuthGuard] },
-	{ path: "item", component: ItemDetailComponent, canActivate: [AuthGuard] },
-	{ path: "login", component: LoginComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
